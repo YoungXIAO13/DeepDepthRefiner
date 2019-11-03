5ed5317a01b16b85fc08e6eedae01ca3dcf51362
Yang XIAO <yangxiao@Yangs-MacBook-Pro.local>
2019-11-03 12:29:44 +0100

    modify interior

--- lib/datasets/interior_net.py
+++ lib/datasets/interior_net.py
@@ -1,6 +1,7 @@
 import os
 from os.path import join
 import cv2
+import pickle
 import torch
 import numpy as np
 import pandas as pd
@@ -9,19 +10,19 @@ import torch.utils.data as data
 
 
 class InteriorNet(data.Dataset):
-    def __init__(self, root_dir, split_name, preprocess=None,
+    def __init__(self, root_dir, preprocess=None, method_name='sharpnet_pred',
                  gt_dir='data', label_dir='label', pred_dir='pred',
                  depth_ext='-depth.png', label_ext='-order-pix.npy'):
         super(InteriorNet, self).__init__()
         self.root_dir = root_dir
-        self.split_name = split_name
         self.preprocess = preprocess
+        self.method_name = method_name
         self.gt_dir = gt_dir
         self.label_dir = label_dir
         self.pred_dir = pred_dir
         self.depth_ext = depth_ext
         self.label_ext = label_ext
-        self.df = pd.read_csv(join(root_dir, '{}.txt'.format(split_name)))
+        self.df = pd.read_csv(join(root_dir, 'InteriorNet.txt'))
 
     def __len__(self):
         return len(self.df)
@@ -39,14 +40,14 @@ class InteriorNet(data.Dataset):
         return depth_gt, depth_pred, label
 
     def _fetch_data(self, index):
-        # fetch depth map and normalize the values into [0, 1)
+        # fetch depth map and normalize the values
         depth_gt_path = join(self.root_dir, self.gt_dir, self.df.iloc[index]['scene'],
                              self.df.iloc[index]['image'] + self.depth_ext)
         depth_gt = cv2.imread(depth_gt_path, -1) / 1000 / 50
 
         depth_pred_path = join(self.root_dir, self.pred_dir, self.df.iloc[index]['scene'],
-                               self.df.iloc[index]['image'] + self.depth_ext)
-        depth_pred = cv2.imread(depth_pred_path, -1) / 1000 / 50
+                               self.method_name, 'data', '{}.pkl'.format(self.df.iloc[index]['image']))
+        depth_pred = pickle.load(depth_pred_path) / 50
 
         # fetch occlusion orientation labels
         label_path = join(self.root_dir, self.label_dir, self.df.iloc[index]['scene'],
@@ -58,5 +59,17 @@ class InteriorNet(data.Dataset):
 
 if __name__ == "__main__":
     root_dir = '/space_sdd/InteriorNet'
-    dataset = InteriorNet(root_dir, 'train')
+    dataset = InteriorNet(root_dir)
     print(len(dataset))
+
+    from torch.utils.data import DataLoader
+    import sys
+    import time
+    test_loader = DataLoader(dataset, batch_size=4, shuffle=False)
+    begin = time.time()
+    for i, data in enumerate(test_loader):
+        data = data
+        print(time.time() - begin)
+        if i == 0:
+            print(data[0].shape, data[1].shape, data[2].shape)
+            sys.exit()

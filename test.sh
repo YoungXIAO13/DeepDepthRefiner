addee712b51fc8ae8d287fd1c17a461da3e2b68e
YoungXIAO13 <appledude1994@gmail.com>
2020-01-16 10:24:17 +0000

    modify structure

--- test.sh
+++ test.sh
@@ -6,11 +6,10 @@ fi
 
 CUDA_VISIBLE_DEVICES=$gpu \
 python -W ignore testing.py \
---checkpoint model/session_ibimis_0/checkpoint_1_1.07.pth \
---val_method eigendnl_vgg \
+--checkpoint models/ibims_depth=1_grad=1_occ=0.01_noContour_useOcc/checkpoint_1_1.08.pth \
+--val_method sharpnet \
 --use_occ --no_contour \
---val_label_dir test_order_nms_pred --th 0.5
-#--val_label_dir label --val_label_ext='-order-pix.npy'
+--val_label_dir ResUnet_test_order_nms_pred_pretrain --th 0.5
 
 
 #--val_label_dir test_order_nms_pred
